e1c166c08b5c9ef51505783547f49ee979942a65
JUN WEI WANG <wjw_03@outlook.com>
2026-04-09 15:36:02 +0800

    Hotfix: patched but in abort

--- core/src/Kokkos_Abort.hpp
+++ core/src/Kokkos_Abort.hpp
@@ -12,6 +12,9 @@
 #ifdef KOKKOS_ENABLE_HIP
 #include <HIP/Kokkos_HIP_Abort.hpp>
 #endif
+#ifdef KOKKOS_ENABLE_MACA
+#include <Maca/Kokkos_Maca_Abort.hpp>
+#endif
 #ifdef KOKKOS_ENABLE_SYCL
 #include <SYCL/Kokkos_SYCL_Abort.hpp>
 #endif
@@ -36,7 +39,8 @@ namespace Impl {
 
 #define KOKKOS_IMPL_ABORT_NORETURN
 
-#elif defined(KOKKOS_ENABLE_HIP) && defined(__HIP_DEVICE_COMPILE__)
+#elif (defined(KOKKOS_ENABLE_HIP) || defined(KOKKOS_ENABLE_MACA)) && \
+    defined(__HIP_DEVICE_COMPILE__)
 // HIP aborts
 #define KOKKOS_IMPL_ABORT_NORETURN [[noreturn]]
 #elif defined(KOKKOS_ENABLE_SYCL) && defined(__SYCL_DEVICE_ONLY__)
@@ -59,13 +63,16 @@ namespace Impl {
 #define KOKKOS_IMPL_ABORT_NORETURN_DEVICE KOKKOS_IMPL_ABORT_NORETURN
 #endif
 
-#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) || \
-    defined(KOKKOS_ENABLE_SYCL) || defined(KOKKOS_ENABLE_OPENACC)
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP) ||     \
+    defined(KOKKOS_ENABLE_MACA) || defined(KOKKOS_ENABLE_SYCL) ||    \
+    defined(KOKKOS_ENABLE_OPENACC) || defined(KOKKOS_ENABLE_NEXTSILICON)
 KOKKOS_IMPL_ABORT_NORETURN_DEVICE inline KOKKOS_IMPL_DEVICE_FUNCTION void
 device_abort(const char *const msg) {
 #if defined(KOKKOS_ENABLE_CUDA)
   ::Kokkos::Impl::cuda_abort(msg);
 #elif defined(KOKKOS_ENABLE_HIP)
+  ::Kokkos::Impl::hip_abort(msg);
+#elif defined(KOKKOS_ENABLE_MACA)
   ::Kokkos::Impl::hip_abort(msg);
 #elif defined(KOKKOS_ENABLE_SYCL)
   ::Kokkos::Impl::sycl_abort(msg);
