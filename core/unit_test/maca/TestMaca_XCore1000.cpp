2d353f96672fdc8957bc21128a05de21cd57f05f
JUN WEI WANG <wjw_03@outlook.com>
2026-04-17 14:10:02 +0800

    perf(simd): enable MACA benchmarks

--- core/unit_test/maca/TestMaca_XCore1000.cpp
+++ core/unit_test/maca/TestMaca_XCore1000.cpp
@@ -7,6 +7,7 @@ import kokkos.core;
 #else
 #include <Kokkos_Core.hpp>
 #endif
+#include <Kokkos_SIMD.hpp>
 #include <TestMaca_Category.hpp>
 
 #include <Maca/Kokkos_Maca_Instance.hpp>
@@ -34,6 +35,18 @@ TEST(maca, random_access_views_do_not_use_raw_pointer_handle) {
   static_assert(!std::is_same_v<handle_t, const int*>);
   static_assert(std::is_same_v<return_t, const int>);
   SUCCEED();
+}
+
+TEST(maca, simd_native_abi_uses_scalar_fallback) {
+  using abi_t = Kokkos::Experimental::simd_abi::Impl::native_abi<
+      double, 0, Kokkos::Maca>;
+  using simd_t = Kokkos::Experimental::basic_simd<double, abi_t>;
+
+  static_assert(std::is_same_v<abi_t, Kokkos::Experimental::simd_abi::scalar>);
+  static_assert(simd_t::size() == 1);
+
+  simd_t value(1.25);
+  EXPECT_DOUBLE_EQ(value[0], 1.25);
 }
 
 }  // namespace Test
